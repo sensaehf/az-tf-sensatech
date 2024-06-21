06ae759141e70e52c62d4c1f50f958f9d358ed7b
Tommi-Sensa <131780500+Tommi-Sensa@users.noreply.github.com>
2024-06-21 16:03:48 +0000

    Add Access policy

--- rg-terraform-stg.tf
+++ rg-terraform-stg.tf
@@ -48,9 +48,25 @@ module "mod_stg_tfstgtommitest" {
 
 
 resource "azurerm_key_vault" "TerraformSecrets" {
-  location = azurerm_resource_group.rg-terraform-stg.location
+  location            = azurerm_resource_group.rg-terraform-stg.location
   resource_group_name = azurerm_resource_group.rg-terraform-stg.name
-  name = "terraformsecrets-sensatech"
-  tenant_id = "${data.azurerm_client_config.current.tenant_id}"
-  sku_name = "standard"
+  name                = "terraformsecrets-sensatech"
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
 }
